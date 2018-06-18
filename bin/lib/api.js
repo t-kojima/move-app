ca6132d734341d85638b8ccc24b92d9c7efdcbcc
t-kojima <t-kojima@hokuichi-e.com>
2018-06-18 17:20:17 +0900

    add: packages

---
+++ bin/lib/api.js
@@ -0,0 +1,87 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+const fetch = require('node-fetch')
+const { promisify } = require('util')
+const config = require('./config')
+
+const ENCODING = 'utf8'
+
+exports.create = async () => {
+  const { domain, app, username, password } = await config.load()
+
+  const url = `https://${domain}/k/v1/preview/app.json`
+  const headers = {
+    'X-Cybozu-Authorization': Buffer.from(`${username}:${password}`).toString(
+      'base64'
+    ),
+    'Content-Type': 'application/json',
+    Host: `${domain}:443`
+  }
+  const body = {
+    name: 'テストテストテスト',
+    space: 1,
+    thread: 1
+  }
+
+  // eslint-disable-next-line no-console
+  console.info(`Fetch api from ${url}`)
+  console.info(headers)
+  console.info(body)
+
+  const response = await fetch(url, { method: 'POST', headers, body })
+  const json = await response.json()
+  if (!response.ok) {
+    throw new Error(json.message)
+  } else {
+    return JSON.stringify(json, null, '  ')
+  }
+}
+
+exports.fetch = async () => {
+  const { domain, app, username, password } = await config.load()
+
+  const fetchApi = async method => {
+    const url = `https://${domain}/k/v1/${method}`
+    const headers = {
+      'X-Cybozu-Authorization': Buffer.from(`${username}:${password}`).toString(
+        'base64'
+      ),
+      Host: `${domain}:443`
+    }
+
+    // eslint-disable-next-line no-console
+    console.info(`Fetch api from ${url}`)
+
+    const response = await fetch(url, { headers })
+    const json = await response.json()
+    if (!response.ok) {
+      throw new Error(json.message)
+    } else {
+      return JSON.stringify(json, null, '  ')
+    }
+  }
+
+  const save = async (target, json) => {
+    const filename = target.replace(/\//g, '_')
+    await promisify(fs.mkdir)('.kintuba').catch(() => {})
+    await promisify(fs.mkdir)('.kintuba/schema').catch(() => {})
+    await promisify(fs.writeFile)(`.kintuba/schema/${filename}`, json, {
+      encoding: ENCODING
+    })
+  }
+
+  const fetchWithSave = async args => {
+    await fetchApi(args.method).then(json => save(args.file, json))
+  }
+  await fetchWithSave({ method: `app.json?id=${app}`, file: 'app.json' })
+  await fetchWithSave({
+    method: `app/views.json?app=${app}`,
+    file: 'views.json'
+  })
+  await fetchWithSave({ method: `form.json?app=${app}`, file: 'form.json' })
+  await fetchWithSave({
+    method: `app/form/fields.json?app=${app}`,
+    file: 'fields.json'
+  })
+}
---
+++ bin/lib/config.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const { promisify } = require('util');
+const { Validator } = require('jsonschema');
+
+const FILE_PATH = '.settings.json';
+const ENCODING = 'utf8';
+
+const schema = {
+  description: 'validation for kintone config json',
+  type: 'object',
+  required: ['domain', 'username', 'password', 'app'],
+  additionalProperties: false,
+  properties: {
+    domain: { type: 'string', pattern: '[a-zA-Z0-9-]+.cybozu.com' },
+    username: { type: 'string' },
+    password: { type: 'string' },
+    app: {
+      type: ['integer', 'array'],
+      items: { type: 'integer' },
+    },
+  },
+};
+
+exports.init = async () => {
+  const data = {
+    domain: '<subdomain>.cybozu.com',
+    app: 0,
+    username: '',
+    password: '',
+  };
+
+  const exists = async (path) => {
+    const result = await promisify(fs.access)(path)
+      .then(() => true)
+      .catch((err) => {
+        if (err.code === 'ENOENT') {
+          return false;
+        }
+        throw err;
+      });
+    return result;
+  };
+
+  if (await exists(FILE_PATH)) {
+    throw new Error(`${FILE_PATH} already exists.`);
+  } else {
+    await promisify(fs.writeFile)(FILE_PATH, JSON.stringify(data, null, '  '), {
+      encoding: ENCODING,
+    });
+  }
+};
+
+exports.load = async () => {
+  const readFile = async () => {
+    const json = await promisify(fs.readFile)(FILE_PATH, ENCODING).catch(() => {
+      throw new Error(`${FILE_PATH} can not read.`);
+    });
+    return JSON.parse(json);
+  };
+
+  const validate = (source) => {
+    const result = new Validator().validate(source, schema);
+    if (result.errors.length > 0) {
+      throw new Error(`${FILE_PATH} is an invalid json schema. [${result.errors[0].message}]`);
+    }
+  };
+
+  const config = await readFile();
+  validate(config);
+  return config;
+};
